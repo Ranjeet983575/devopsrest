976430145e1d803540fd1fcdfb259d5e10bc019f
rkprajapati404 <rkprajapati404@gmail.com>
2020-10-11 20:34:52 +0530

    create devopsrest

---
+++ src/main/java/com/example/devopsrest/DevopsrestApplication.java
@@ -0,0 +1,13 @@
+package com.example.devopsrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsrestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/devopsrest/DevopsrestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devopsrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsrestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
