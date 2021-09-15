b2d6cc53739aef077ea5bfd556afda69cb53fd54
Ranjeet Kumar Prajapati <ranjeet983575@gmail.com>
2021-09-16 01:05:45 +0530

    fix 404

--- src/main/java/com/example/devopsrest/DevopsrestApplication.java
+++ src/main/java/com/example/devopsrest/DevopsrestApplication.java
@@ -3,9 +3,10 @@ package com.example.devopsrest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DevopsrestApplication {
+public class DevopsrestApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevopsrestApplication.class, args);
