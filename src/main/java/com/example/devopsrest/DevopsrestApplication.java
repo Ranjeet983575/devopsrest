b1e2b272a2c57fc61c9e40d51c36d0a8de5da2d4
Ranjeet983575 <ranjeet983575@gmail.com>
2021-09-16 00:24:48 +0530

    Update DevopsrestApplication.java

--- src/main/java/com/example/devopsrest/DevopsrestApplication.java
+++ src/main/java/com/example/devopsrest/DevopsrestApplication.java
@@ -2,6 +2,7 @@ package com.example.devopsrest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class DevopsrestApplication {
