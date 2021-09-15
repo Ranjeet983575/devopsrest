2beb513433a9ed843c8ebf104e48ab8f574ff7f3
Ranjeet Kumar Prajapati <ranjeet983575@gmail.com>
2021-09-16 00:19:43 +0530

    fix 404

--- src/main/java/com/example/devopsrest/DevopsrestApplication.java
+++ src/main/java/com/example/devopsrest/DevopsrestApplication.java
@@ -2,6 +2,7 @@ package com.example.devopsrest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class DevopsrestApplication {
@@ -10,4 +11,8 @@ public class DevopsrestApplication {
 		SpringApplication.run(DevopsrestApplication.class, args);
 	}
 
+
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DevopsrestApplication.class);
+	}
 }
